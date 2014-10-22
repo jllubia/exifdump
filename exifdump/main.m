953a385a28b6b96959fc90a348632df191f86257
Jorge Llubia <jll@latenitesoft.com>
2014-10-22 20:39:37 +0200

    Initial Commit

---
+++ exifdump/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  exifdump
+//
+//  Created by Jorge on 22/10/14.
+//  Copyright (c) 2014 Jorge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
